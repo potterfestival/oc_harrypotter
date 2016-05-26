8836b730d8b3b6160eccbc94a6ff10e6072e6a36
Tobias F. Petersen <tobsf@hotmail.com>
2016-05-26 10:26:19 +0200

    styling

--- template.php
+++ template.php
@@ -4,6 +4,7 @@
  */
 function oc_harrypotter_preprocess_html(&$vars) { 
  oc_harrypotter_oc_custom_backgrounds();
+ drupal_add_css(drupal_get_path('theme', 'oc_harrypotter') . '/css/view-hp-events.css');
 }
 
 /*
