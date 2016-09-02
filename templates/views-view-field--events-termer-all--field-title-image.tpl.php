470db019a7ea636f8cbef6cf6f077710369dcc9f
Tobias F. Petersen <tobsf@hotmail.com>
2016-09-02 09:02:20 +0200

    view templates

---
+++ templates/views-view-field--events-termer-all--field-title-image.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo $output;
+if(isset($row->field_field_title_image[0]['raw']['field_foto_kilde_']['und'][0]['value']))
+{
+    echo "<span class='oc_photo_kilde'>Foto: ".$row->field_field_title_image[0]['raw']['field_foto_kilde_']['und'][0]['value']."</span>";
+}
