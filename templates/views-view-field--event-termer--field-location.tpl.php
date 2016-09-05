b980fdc77e85f37163efd6c30b3a970e2898f9bf
Tobias F. Petersen <tobsf@hotmail.com>
2016-09-05 11:02:51 +0200

    template

--- templates/views-view-field--event-termer--field-location.tpl.php
+++ templates/views-view-field--event-termer--field-location.tpl.php
@@ -1,3 +1,3 @@
 <?php
-echo $row->field_field_location[0]['raw']['name_line'] . ", " . $row->field_field_location[0]['raw']['thoroughfare'];
+echo "<a href='http://maps.google.com/?saddr=Current%20Location&daddr=".$row->field_field_location[0]['raw']['thoroughfare']."'>" . $row->field_field_location[0]['raw']['name_line'] . ", " . $row->field_field_location[0]['raw']['thoroughfare'] . "</a>";
 
