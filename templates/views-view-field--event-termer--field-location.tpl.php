0c2abcf2c312353de95b83064257a79f6aa90f19
Tobias F. Petersen <tobsf@hotmail.com>
2016-09-05 11:34:52 +0200

    template

--- templates/views-view-field--event-termer--field-location.tpl.php
+++ templates/views-view-field--event-termer--field-location.tpl.php
@@ -1,16 +1,16 @@
 <?php
 $gmapAddr = $row->field_field_location[0]['raw']['thoroughfare'] . ", " . $row->field_field_location[0]['raw']['postal_code'] . " " . $row->field_field_location[0]['raw']['locality'];
-$user_agent = $useragent = $_SERVER ['HTTP_USER_AGENT'];
+$user_agent = $_SERVER ['HTTP_USER_AGENT'];
 $link = "";
-if(strpos($a, 'iPad') != false || strpos($a, 'iPhone') != false)
+if(strpos($user_agent, 'iPad') != false || strpos($user_agent, 'iPhone') != false)
 {
     $link = "http://maps.apple.com/?saddr=Current%20Location&daddr=" . $gmapAddr;
 }
-elseif(strpos($a, 'Android') != false)
+elseif(strpos($user_agent, 'Android') != false)
 {
     $link = "http://maps.google.com/?saddr=Current%20Location&daddr=" . $gmapAddr;
 }
-elseif(strpos($a, 'Windows Phone') != false || strpos($a, 'IEMobile') != false)
+elseif(strpos($user_agent, 'Windows Phone') != false || strpos($user_agent, 'IEMobile') != false)
 {
     $link = "maps:" . $gmapAddr;
 }
