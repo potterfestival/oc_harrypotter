107f09a0c5fa9fee2925d29b74d08e80c96412bc
Tobias F. Petersen <tobsf@hotmail.com>
2016-09-05 12:05:57 +0200

    reuse js to make map lins for devices.

--- templates/views-view-field--event-termer--field-location.tpl.php
+++ templates/views-view-field--event-termer--field-location.tpl.php
@@ -1,22 +1,10 @@
+ <i class="icon-margin fa fa-map-marker fa-2x"></i>
+<span id="map" >
+  <?php print $row->field_field_location[0]['raw']['thoroughfare']; ?>
+</span>
+<span id="map_data" style="display:none;">
+  <?php print $row->field_field_location[0]['raw']['thoroughfare']; ?>
+</span>
 <?php
-$gmapAddr = $row->field_field_location[0]['raw']['thoroughfare'] . ", " . $row->field_field_location[0]['raw']['postal_code'] . " " . $row->field_field_location[0]['raw']['locality'];
-$user_agent = $_SERVER ['HTTP_USER_AGENT'];
-$link = "";
-if(strpos($user_agent, 'iPad') != false || strpos($user_agent, 'iPhone') != false)
-{
-    $link = "http://maps.apple.com/?saddr=Current%20Location&daddr=" . $gmapAddr;
-}
-elseif(strpos($user_agent, 'Android') != false && strpos($user_agent, 'Windows Phone') == false)
-{
-    $link = "http://maps.google.com/?saddr=Current%20Location&daddr=" . $gmapAddr;
-}
-elseif(strpos($user_agent, 'Windows Phone') != false || strpos($user_agent, 'IEMobile') != false)
-{
-    $link = "bingmaps:?q=" . $gmapAddr;
-}
-else
-{
-    $link = "http://maps.google.com/?saddr=Current%20Location&daddr=" . $gmapAddr;
-}
-echo "<a target='_blank' href='".$link."'>" . $row->field_field_location[0]['raw']['thoroughfare'] . "</a>";
+
 
