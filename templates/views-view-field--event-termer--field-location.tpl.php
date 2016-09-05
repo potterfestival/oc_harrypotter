7e152e5d05959c5ca8f2c138f22022a9f9d6f1a9
Tobias F. Petersen <tobsf@hotmail.com>
2016-09-05 11:21:52 +0200

    template

--- templates/views-view-field--event-termer--field-location.tpl.php
+++ templates/views-view-field--event-termer--field-location.tpl.php
@@ -1,4 +1,22 @@
 <?php
 $gmapAddr = $row->field_field_location[0]['raw']['thoroughfare'] . ", " . $row->field_field_location[0]['raw']['postal_code'] . " " . $row->field_field_location[0]['raw']['locality'];
-echo "<a target='_blank' href='http://maps.google.com/?saddr=Current%20Location&daddr=".$gmapAddr."'>" . $row->field_field_location[0]['raw']['thoroughfare'] . "</a>";
+$user_agent = $useragent = $_SERVER ['HTTP_USER_AGENT'];
+$link = "";
+if(strpos($a, 'iPad') !== false || strpos($a, 'iPhone') !== false)
+{
+    $link = "http://maps.apple.com/?saddr=Current%20Location&daddr=" . $gmapAddr;
+}
+elseif(strpos($a, 'Android') !== false)
+{
+    $link = "http://maps.google.com/?saddr=Current%20Location&daddr=" . $gmapAddr;
+}
+elseif(strpos($a, 'Windows Phone') !== false || strpos($a, 'IEMobile') !== false)
+{
+    $link = "maps:" . $gmapAddr;
+}
+else
+{
+    $link = "http://maps.google.com/?saddr=Current%20Location&daddr=" . $gmapAddr;
+}
+echo "<a target='_blank' href='".$link."'>" . $row->field_field_location[0]['raw']['thoroughfare'] . "</a>";
 
