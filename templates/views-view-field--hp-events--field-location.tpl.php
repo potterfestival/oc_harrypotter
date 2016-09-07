fa1173911553b1d2cad85b97d1809ad365e4fb3f
Tobias F. Petersen <tobsf@hotmail.com>
2016-09-07 14:02:59 +0200

    template

---
+++ templates/views-view-field--hp-events--field-location.tpl.php
@@ -0,0 +1,9 @@
+<div class="List-map-link">
+    <span id="map" >
+      <?php print $row->field_field_location[0]['raw']['thoroughfare']; ?>
+    </span>
+    <span id="map_data" style="display:none;">
+      <?php print $row->field_field_location[0]['raw']['thoroughfare'] . ", " . $row->field_field_location[0]['raw']['postal_code'] . " " . $row->field_field_location[0]['raw']['locality']; ?>
+    </span>
+</div>
+<?php
